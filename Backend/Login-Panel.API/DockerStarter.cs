aed14f6202b9bb03cad86c60ad29974f7d537318
Jan Chojnacki <jan@chojnacki.dev>
2025-04-05 16:07:34 +0200

    start or create database docker on application start

---
+++ Backend/Login-Panel.API/DockerStarter.cs
@@ -0,0 +1,41 @@
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace Login_Panel.API;
+
+public class DockerStarter
+{
+    public async Task StartPostgresAsync()
+    {
+        var client = new DockerClientConfiguration().CreateClient();
+
+        var containers = await client.Containers.ListContainersAsync(new ContainersListParameters
+        {
+            All = true
+        });
+
+        var existingContainer = containers.FirstOrDefault(c => c.Names.Any(n => n.TrimStart('/') == "panel-db"));
+
+        if (existingContainer != null && !existingContainer.State.Equals("running", StringComparison.OrdinalIgnoreCase))
+        {
+            await client.Containers.StartContainerAsync(existingContainer.ID, new ContainerStartParameters());
+            return;
+        }
+
+        await client.Containers.CreateContainerAsync(new CreateContainerParameters
+        {
+            Image = "postgres",
+            Name = "panel-db",
+            Env = new List<string> { "POSTGRES_PASSWORD=admin" },
+            HostConfig = new HostConfig
+            {
+                PortBindings = new Dictionary<string, IList<PortBinding>>
+                {
+                    ["5432/tcp"] = new List<PortBinding> { new PortBinding { HostPort = "55444" } }
+                }
+            }
+        });
+
+        await client.Containers.StartContainerAsync("panel-db", new ContainerStartParameters());
+    }
+}
